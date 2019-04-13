99068a7359834ffa3d920cb4de06340e7bdfddd4
behnam <behnam.asadi@gmail.com>
2019-04-13 17:24:34 +0200

    Travsi added

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
-# cropping_pointcloud
-This ROS package enables you to crop the scene from Kinect (input topic type: pcl pointcloud). You can even enable fitting a plane to remove the ground from the scene and by adjusting correct parameter you can get the desired object from the scene.
+# Cropping Pointcloud
+This ROS package enables you to crop the scene from Kinect (input topic type: pcl pointcloud) with ROS dynamic reconfigure. You can even enable fitting a plane to remove the ground from the scene and by adjusting correct parameter you can get the desired object from the scene.
 
 
 ![GUI in dynamic reconfigure ](/images/cropping_pointcloud_dynamic_reconfigure_gui.jpg)
 ![White dots are original scene and rgb dots are from the cropped cloud. Values for the volume of cuboid are coming from sliders.](/images/cropping_pointcloud_rviz.jpg)
 
 
-
-
-
-
+![alt text](https://img.shields.io/badge/license-BSD-blue.svg)
+[![Build Status](https://travis-ci.org/behnamasadi/cropping_pointcloud.svg?branch=master)](https://travis-ci.org/behnamasadi/cropping_pointcloud)
--- src/src/cropping_pointcloud.cpp
+++ src/src/cropping_pointcloud.cpp
@@ -55,13 +55,6 @@ void paramCallback(rmal::croppingCloudConfig &config, uint32_t level)
 }
 
 
-
-//sensor_msgs::PointCloud2 message to a sensor_msgs::PointCloud messa
-//    sensor_msgs::convertPointCloud2ToPointCloud()
-
-//Convert a sensor_msgs::PointCloud message to a sensor_msgs::PointCloud2 message.
-//    sensor_msgs::convertPointCloudToPointCloud2.
-
 void extractObject(pcl::PointCloud<PointType>::Ptr cropped_cloud_ptr)
 {
     no_plane_cloud.reset(new pcl::PointCloud<PointType>);
@@ -98,70 +91,8 @@ void extractObject(pcl::PointCloud<PointType>::Ptr cropped_cloud_ptr)
     planar_inliers_extraction.setIndices (planar_inliers);
     planar_inliers_extraction.setNegative (true);
     planar_inliers_extraction.filter (*no_plane_cloud);
-
-
-
     std::vector<int> no_Nan_vector;
     pcl::removeNaNFromPointCloud(*no_plane_cloud,*no_plane_cloud,no_Nan_vector);
-
-
-/*
-    // Segmentation object
-
-    // KdTree object
-    pcl::search::KdTree<PointType>::Ptr segmentation_tree (new pcl::search::KdTree<PointType>);
-    // Euclidean Cluster Extraction object
-    pcl::EuclideanClusterExtraction<PointType> cluster_exctraction;
-    // Cluster indices vector
-    std::vector<pcl::PointIndices> clusters_indices_vector;
-    // Vector containing updated folders
-    segmentation_tree->setInputCloud (no_plane_cloud);
-    // Cluster extraction object initializations
-    //    cluster_exctraction.setClusterTolerance (0.05);
-    cluster_exctraction.setClusterTolerance (0.015  );
-
-    cluster_exctraction.setMinClusterSize (50);
-    cluster_exctraction.setSearchMethod (segmentation_tree);
-    cluster_exctraction.setInputCloud (no_plane_cloud);
-
-    // Cluster extraction
-    // Each element of the vector <clusters_indices_vector> corresponds to a cluster of the cloud
-    cluster_exctraction.extract (clusters_indices_vector);
-
-    int number_of_clusters = clusters_indices_vector.size();
-
-    //pcl::console::print_error ("\tdone\n");
-//    std::cout << "\t" <<  number_of_clusters << " clusters found" << std::endl;
-
-
-
-    pcl::PointIndices main_cluster=clusters_indices_vector.at(0);
-
-
-    object_cluster.reset(new pcl::PointCloud<PointType>);
-
-    // Loading of cluster informations and data from the original (segmented) cloud
-    for (std::size_t i=0; i<main_cluster.indices.size();i++)
-    {
-        object_cluster->points.push_back (no_plane_cloud->points[main_cluster.indices.at(i)]);
-    }
-    object_cluster->width = object_cluster->points.size ();
-    object_cluster->height = 1;
-    object_cluster->is_dense = true;
-
-*/
-
-//    std::string file_name="spary.pcd";
-//    pcl::io::savePCDFileASCII (file_name, *no_plane_cloud );
-
-
-
-
-//    std::string file_name="spary.ply";
-//    pcl::io::savePLYFile(file_name,*no_plane_cloud);
-
-
-
 }
 
 
@@ -246,7 +177,6 @@ double x_min, double x_max,double  y_min,double  y_max,double  z_min,double z_ma
 
         surface_hull.reset(new pcl::PointCloud<PointT>);
         hull.reconstruct(*surface_hull, polygons);
-        //    std::cout << surface_hull->size() << std::endl;
 
     }catch( std::exception e)
     {
@@ -274,6 +204,7 @@ double x_min, double x_max,double  y_min,double  y_max,double  z_min,double z_ma
 
 
 }
+
 
 template<>
 void croppingCloud(boost::shared_ptr<pcl::PointCloud<pcl::PointXYZRGB> > input_cloud_ptr, boost::shared_ptr<pcl::PointCloud<pcl::PointXYZRGB> > output_cloud_ptr,
@@ -351,9 +282,6 @@ double x_min, double x_max,double  y_min,double  y_max,double  z_min,double z_ma
 
     boost::shared_ptr<pcl::PointCloud<pcl::PointXYZRGB> > surface_hull (new pcl::PointCloud<pcl::PointXYZRGB>);
     hull.reconstruct(*surface_hull, polygons);
-    //    std::cout << surface_hull->size() << std::endl;
-
-
 
     pcl::CropHull<pcl::PointXYZRGB> bb_filter;
 
@@ -367,89 +295,43 @@ double x_min, double x_max,double  y_min,double  y_max,double  z_min,double z_ma
 }
 
 
-
-
 void pointcloudCallback(const sensor_msgs::PointCloud2ConstPtr& pointcloud_msg)
 {
-
-
-
     received_cloud_ptr.reset(new pcl::PointCloud<PointType>);
     pcl::fromROSMsg(*pointcloud_msg.get(), *received_cloud_ptr.get());
-//    std::cout<<"pointcloud_msg->header.frame_id: "<<pointcloud_msg->header.frame_id <<std::endl;
-
-
-
-
     cropped_cloud_ptr.reset(new pcl::PointCloud<PointType>);
-
     croppingCloud(received_cloud_ptr, cropped_cloud_ptr,x_max,x_min, y_max, y_min,z_max,z_min);
-
     pcl::toROSMsg(*cropped_cloud_ptr.get(),cropped_cloud_msg );
     extractObject(cropped_cloud_ptr);
-
     pcl::toROSMsg(*no_plane_cloud.get(),object_msg );
-
-//    pcl::toROSMsg(*object_cluster.get(),object_msg );
-
-
-
 }
-
-
 
 
 int main(int argc, char** argv)
 {
     ros::init(argc,argv,"croppingCloud");
     ros::NodeHandle nh;
-//    std::string pointcloud_topic="/kinect2/depth_scaled/points";
-//    std::string pointcloud_topic="/kinect2/depth_scaled_registered/points";
-    //std::string pointcloud_topic="/kinect2/sd/points";
-	//camera/rgb/points
-	std::string pointcloud_topic="camera/rgb/points";
+    std::string pointcloud_topic="camera/rgb/points";
     ros::Subscriber sub_cloud= nh.subscribe<sensor_msgs::PointCloud2>(pointcloud_topic,1 , pointcloudCallback);
     ros::Publisher marker_pub=nh.advertise<visualization_msgs::Marker>("visualization_marker",10);
     ros::Publisher pub_cropped_cloud=nh.advertise<sensor_msgs::PointCloud2>("cropped_cloud",1);
     ros::Publisher pub_object_cloud=nh.advertise<sensor_msgs::PointCloud2>("object_cloud",1);
 
-
-
-
     dynamic_reconfigure::Server<rmal::croppingCloudConfig> server;
     dynamic_reconfigure::Server<rmal::croppingCloudConfig>::CallbackType f;
-
     f = boost::bind(&paramCallback, _1, _2);
     server.setCallback(f);
-
-//    cropped_cloud_ptr->header.frame_id = "head_mount_kinect2_link";
     ros::Rate loop_rate(30);
-
-//    x_min=-0.3;
-//    x_max=0.3;
-//    y_min=-1.0;
-//    y_max=1.5;
-//    z_min=-1;
-//    z_max=1;
 
     while(ros::ok)
     {
 
         ros::spinOnce();
-		std::string frame_id="openni_rgb_optical_frame";
-
-//        object_msg.header.frame_id = "kinect2_ir_optical_frame";
-//        cropped_cloud_msg.header.frame_id="kinect2_ir_optical_frame";
-
+	std::string frame_id="openni_rgb_optical_frame";
         object_msg.header.frame_id = frame_id;
         cropped_cloud_msg.header.frame_id=frame_id;
-
-		
-		
-		
         pub_cropped_cloud.publish (cropped_cloud_msg);
         pub_object_cloud.publish (object_msg);
-
         loop_rate.sleep();
     }
     return 0;
